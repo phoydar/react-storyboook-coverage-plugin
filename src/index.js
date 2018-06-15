cce368d93b7566f3d60d581e6a43373a194edd22
Patrick Hoydar <patrick.hoydar@salesloft.com>
2018-06-15 14:51:25 -0400

    prettier and linter added

--- src/index.js
+++ src/index.js
@@ -47,19 +47,19 @@ module.exports = () => {
   storybookFileExportedFileDifference = storyWithNoExport.length
   coveragePercentage = round((exportedAndHasStory.length / exportedModules.length) * 100, 2)
 
-  coverageStyling = coveragePercentage < 100 ? 'bold.red' : 'bold.green'
+  coverageStyling = coveragePercentage < config.passingCoveragePercentage ? 'bold.red' : 'bold.green'
 
-  console.log(chalk`
+  console.log(chalk `
 {bold Storybook Coverage Report}
 --------------------------------------
+{${coverageStyling} Storybook Coverage: ${coveragePercentage}}
 Storybook Files: {bold ${allStoryBookFiles.length}}
 Exported modules: {bold ${exportedModules.length}}
 Exported and has a story: {bold ${exportedAndHasStory.length}}
-{${coverageStyling} Storybook Coverage: ${coveragePercentage}}
   `)
 
   if (uncoveredComponents) {
-    console.log(chalk`
+    console.log(chalk `
 {bold Modules without stories}
 --------------------------------------
 ${uncoveredComponents.join('\n')}
@@ -67,15 +67,15 @@ ${uncoveredComponents.join('\n')}
   }
 
   if (storybookFileExportedFileDifference) {
-    console.warn(chalk`
-{yellow.bold You have ${storybookFileExportedFileDifference} Storybook file(s) \nwith no corresponding export!}
+    console.warn(chalk `
+{yellow.bold You have ${storybookFileExportedFileDifference} Storybook file(s) \nwith no corresponding export}
 --------------------------------------
 ${storyWithNoExport.join('\n')}
     `)
   }
 
-  if (coveragePercentage < 100) {
-    console.log(chalk`{white.bold.bgRed  STORYBOOK COVERAGE MUST BE 100% IN ORDER TO PUSH TO REMOTE!!! }
+  if (coveragePercentage < config.passingCoveragePercentage) {
+    console.log(chalk `{white.bold.bgRed  STORYBOOK COVERAGE MUST BE 100% IN ORDER TO PUSH TO REMOTE!!! }
 Use the flag {bold.magenta  --no-verify } to override the pre-push hook
     `)
     process.exit(1)
